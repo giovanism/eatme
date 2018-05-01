bc26069747f8c0a852bb55ed2df7a15b1aa97638
Chuyang Liu <chuyang.s.liu@gmail.com>
2018-05-01 20:35:55 +0800

    Update client: write es6 gulpfile

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,26 +1,24 @@
 'use strict'
 
-var del = require('del')
-var runSequence = require('run-sequence')
-var gulp = require('gulp')
-var gulpif = require('gulp-if-else')
-var htmlmin = require('gulp-htmlmin')
-var csso = require('gulp-csso')
-var uglify = require('gulp-uglify')
-var babel = require('gulp-babel')
-var browserify = require('gulp-browserify')
-var concat = require('gulp-concat')
-var sourcemaps = require('gulp-sourcemaps')
+const del = require('del')
+const runSequence = require('run-sequence')
+const gulp = require('gulp')
+const gulpif = require('gulp-if-else')
+const htmlmin = require('gulp-htmlmin')
+const csso = require('gulp-csso')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const browserify = require('gulp-browserify')
+const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
 
-function isDev() {
-  return !process.env.NODE_ENV || process.env.NODE_ENV === 'dev'
-}
+const isDev = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'dev'
 
-gulp.task('clean', function() {
+gulp.task('clean', () => {
   return del(['dist'])
 })
 
-gulp.task('pages', function() {
+gulp.task('pages', () => {
   return gulp.src('src/pages/**/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true,
@@ -29,30 +27,30 @@ gulp.task('pages', function() {
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('images', function() {
+gulp.task('images', () => {
   return gulp.src('src/assets/images/**/*')
     .pipe(gulp.dest('dist/assets/images'))
 })
 
-gulp.task('fonts', function() {
+gulp.task('fonts', () => {
   return gulp.src('src/assets/fonts/**/*')
     .pipe(gulp.dest('dist/assets/fonts'))
 })
 
-gulp.task('css-core', function() {
+gulp.task('css-core', () => {
   return gulp.src(['src/assets/css/core/font.css', 'src/assets/css/core/**/*.css'])
     .pipe(csso())
     .pipe(concat('index.css'))
     .pipe(gulp.dest('dist/assets/css'))
 })
 
-gulp.task('css-lib', function() {
+gulp.task('css-lib', () => {
   return gulp.src('src/assets/css/lib/**/*.css')
     .pipe(concat('lib.css'))
     .pipe(gulp.dest('dist/assets/css'))
 })
 
-gulp.task('js-core', function() {
+gulp.task('js-core', () => {
   return gulp.src('src/assets/js/core/index.js')
     .pipe(browserify())
     .pipe(babel({
@@ -70,13 +68,13 @@ gulp.task('js-core', function() {
     .pipe(gulp.dest('dist/assets/js'))
 })
 
-gulp.task('js-lib', function() {
+gulp.task('js-lib', () => {
   return gulp.src(['src/assets/js/lib/jquery.min.js', 'src/assets/js/lib/**/*.js'])
     .pipe(concat('lib.js'))
     .pipe(gulp.dest('dist/assets/js'))
 })
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch('src/pages/**/*.html', ['pages'])
   gulp.watch('src/assets/images/**/*', ['images'])
   gulp.watch('src/assets/fonts/**/*', ['fonts'])
@@ -86,7 +84,7 @@ gulp.task('watch', function() {
   gulp.watch('src/assets/js/lib/**/*.js', ['js-lib'])
 })
 
-gulp.task('build', function() {
+gulp.task('build', () => {
   runSequence.options.ignoreUndefinedTasks = true
   runSequence('clean', 'pages', 'images', 'fonts', 'css-core',
     'css-lib', 'js-core', 'js-lib', isDev() ? 'watch' : '')
