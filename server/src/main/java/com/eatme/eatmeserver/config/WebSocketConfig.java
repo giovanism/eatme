31cb640b07c8e72ec165a9b5c6d0eb81cff83722
Chuyang Liu <chuyang.s.liu@gmail.com>
2018-05-08 23:55:09 +0800

    [nginx] update log format and buffer flush & [server] add webskt allow origins

--- server/src/main/java/com/eatme/eatmeserver/config/WebSocketConfig.java
+++ server/src/main/java/com/eatme/eatmeserver/config/WebSocketConfig.java
@@ -10,13 +10,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private static final String CLIENT_LIB_URL =
+        "https://cdn.jsdelivr.net/npm/sockjs-client@1.1.4/dist/sockjs.min.js";
+
     private static final String BASE = "/ws";
     private static final String ENDPOINT = BASE + "/ep";
     public static final String PREFIX_SUBSCRIBE = BASE + "/sb";
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint(ENDPOINT).withSockJS();
+        registry.addEndpoint(ENDPOINT)
+            .setAllowedOrigins("*")
+            .withSockJS()
+            .setClientLibraryUrl(CLIENT_LIB_URL);
     }
 
     @Override
