2105a8fcdf7cc4646242f03f02314fec907d137a
Chuyang Liu <chuyang.s.liu@gmail.com>
2018-05-11 11:29:40 +0800

    [server] use lettuce connection pool

--- server/src/main/java/com/eatme/eatmeserver/config/RedisConfig.java
+++ server/src/main/java/com/eatme/eatmeserver/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.eatme.eatmeserver.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SuppressWarnings("unused")
@@ -25,13 +27,33 @@ public class RedisConfig {
     @Value("${redis.port}")
     private int port;
 
+    @Value("${redis.pool.max-total}")
+    private int maxTotal;
+
+    @Value("${redis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${redis.pool.min-idle}")
+    private int minIdle;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory();
-        factory.setDatabase(database);
-        RedisStandaloneConfiguration conf = factory.getStandaloneConfiguration();
-        conf.setHostName(host);
-        conf.setPort(port);
+        GenericObjectPoolConfig objPoolConfig = new GenericObjectPoolConfig();
+        objPoolConfig.setMaxTotal(maxTotal);
+        objPoolConfig.setMaxIdle(maxIdle);
+        objPoolConfig.setMinIdle(minIdle);
+
+        LettucePoolingClientConfiguration poolClientConfig =
+            LettucePoolingClientConfiguration.builder().poolConfig(objPoolConfig).build();
+
+        RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration();
+        standaloneConfig.setDatabase(database);
+        standaloneConfig.setHostName(host);
+        standaloneConfig.setPort(port);
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(standaloneConfig, poolClientConfig);
+        factory.setShareNativeConnection(true);
+
         log.info("Creating bean \'redisConnectionFactory\'");
         return factory;
     }
